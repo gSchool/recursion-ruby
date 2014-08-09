fc1f4e0bc76c9b828b6291fe1a76228561ea3e7d
Evan Tedesco <Evantedesco@gmail.com>
2014-08-08 21:21:44 -0600

    names are added with indentation

--- lib/tree.rb
+++ lib/tree.rb
@@ -10,11 +10,20 @@ class Tree
   end
 
   def names(node)
-    name_array = []
-    name_array << node.name
+    name_array = [node.name]
     node.children.map do |child|
       name_array += names(child)
     end
     name_array
+  end
+
+  def names_with_indentation(node)
+    multiplier = 0
+    indented_node = node.name + ("  " * multiplier)
+    name_array = [indented_node]
+    node.children.each do |child|
+      multiplier += 1
+      name_array += names_with_indentation(child)
+    end
   end
 end
