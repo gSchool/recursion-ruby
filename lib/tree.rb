1bc8f62e4c2c280178c28955df5f92df2bd53998
Evan Tedesco <Evantedesco@gmail.com>
2014-08-08 21:15:22 -0600

    it returns the names in an array

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,10 +2,19 @@ require 'node'
 
 class Tree
 
-  def print_names(name)
-    puts name.name
-    name.children.each do |child|
+  def print_names(node)
+    puts node.name
+    node.children.each do |child|
       print_names(child)
     end
+  end
+
+  def names(node)
+    name_array = []
+    name_array << node.name
+    node.children.map do |child|
+      name_array += names(child)
+    end
+    name_array
   end
 end
