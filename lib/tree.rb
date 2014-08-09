f8f56c889beb45ab8f857986f66d3ae428336b19
Evan Tedesco <Evantedesco@gmail.com>
2014-08-08 22:11:12 -0600

    it converts names to a nested hash

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,10 +19,17 @@ class Tree
 
   def names_with_indentation(node, multiplier = 0)
     name_array = ["#{("  " * multiplier)}#{node.name}"]
-
     node.children.each do |child|
       name_array += names_with_indentation(child, multiplier + 1)
     end
     name_array
+  end
+
+  def to_hash(node)
+    name_hash = {node.name => {}}
+    node.children.each do |child|
+      name_hash[node.name].merge!(to_hash(child))
+    end
+    name_hash
   end
 end
